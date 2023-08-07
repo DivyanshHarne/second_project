f49758b3952df124aded91095caea5685e5f6a4f
Divyansh Harne <divyanshsanjayharne23@gmail.com>
2023-08-07 21:14:34 +0530

    Day 2

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+ import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int day = 21;
+    final String name = "Divyansh Harne";
+    double pi = 3.14;
+    bool isMale = true;
+    num temp = 35;
+    var numb = "Thursday";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $day Days of Code by ${name}"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
